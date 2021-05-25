9305870eed1c0019a4e869ef41106e6d02416042
romanjin <romanjin@tencent.com>
2021-05-25 15:29:01 +0800

    3-9创建class类组件

---
+++ src/components/Robot.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import styles from "./Robot.module.css";
+
+interface RobotProps {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+    return (
+        <div className={styles.cardContainer}>
+            <img src={`https://robohash.org/${id}`} alt="" />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+};
+
+export default Robot;
